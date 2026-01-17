e31be682d8493625d177a7b51ec832d6478c8553
aolindapur <artoflivingindapur@gmail.com>
2026-01-17 17:38:45 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@
 // - Mobile menu
 // - Form validation
 // - Event registration
+function openModal(src) {
+  document.getElementById("modalImg").src = src;
+  document.getElementById("imgModal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("imgModal").style.display = "none";
+}
