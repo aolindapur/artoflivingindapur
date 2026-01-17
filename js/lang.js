a59e3da22f0b89a2d5f2e53735e60a730b87d46b
aolindapur <artoflivingindapur@gmail.com>
2026-01-17 18:41:27 +0530

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -1,19 +1,23 @@
-const switcher = document.getElementById("languageSwitcher");
+document.addEventListener("DOMContentLoaded", () => {
+  const switcher = document.getElementById("languageSwitcher");
 
-function loadLang(lang) {
-  fetch(`lang/${lang}.json`)
-    .then(res => res.json())
-    .then(data => {
-      document.querySelectorAll("[data-lang]").forEach(el => {
-        el.innerHTML = data[el.dataset.lang];
-      });
-      localStorage.setItem("lang", lang);
-    });
-}
+  function loadLang(lang) {
+    fetch(`lang/${lang}.json`)
+      .then(res => res.json())
+      .then(data => {
+        document.querySelectorAll("[data-lang]").forEach(el => {
+          const key = el.getAttribute("data-lang");
+          el.innerHTML = data[key] || "";
+        });
+        localStorage.setItem("lang", lang);
+      })
+      .catch(err => console.error("Language load error:", err));
+  }
 
-const savedLang = localStorage.getItem("lang") || "en";
-if (switcher) {
-  switcher.value = savedLang;
-  loadLang(savedLang);
-  switcher.addEventListener("change", () => loadLang(switcher.value));
-}
+  const savedLang = localStorage.getItem("lang") || "en";
+  if (switcher) {
+    switcher.value = savedLang;
+    loadLang(savedLang);
+    switcher.addEventListener("change", () => loadLang(switcher.value));
+  }
+});
